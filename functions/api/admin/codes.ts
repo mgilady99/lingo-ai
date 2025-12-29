d9a9114648b097ce97cab40554c9201d57821725
mgilady99 <callilcoil@gmail.com>
2025-12-29 23:49:30 +0200

    Delete functions directory

--- functions/api/admin/codes.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { env } = context;
-  try {
-    const result = await env.DB.prepare("SELECT * FROM promo_codes ORDER BY is_used ASC, code ASC").all();
-    return new Response(JSON.stringify(result.results || []));
-  } catch (e) {
-    return new Response(JSON.stringify([])); 
-  }
-}
